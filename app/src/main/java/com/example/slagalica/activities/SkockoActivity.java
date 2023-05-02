e855d38107ef25e07ad0155637ed7dfeda834ea3
flower <natasakotaranin1@gmail.com>
2023-05-02 17:07:03 +0200

    igre

---
+++ app/src/main/java/com/example/slagalica/activities/SkockoActivity.java
@@ -0,0 +1,14 @@
+package com.example.slagalica.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SkockoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skocko);
+    }
+}
